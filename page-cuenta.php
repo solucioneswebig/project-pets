60512499f60bbceee84adc37e1e43c785a9e8cee
solucioneswebig <alexanderr677@gmail.com>
2020-04-19 20:15:03 -0400

    Modified user data extra

--- page-cuenta.php
+++ page-cuenta.php
@@ -41,11 +41,12 @@ $fondo = "background: #000 !important;";
             <div class="col-md-3">
             <div class="list-group">
             <a href="<?php echo get_site_url(); ?>/mi-cuenta/" class="list-group-item list-group-item-action <?php if(isset($_GET["slug"])){ }else{ echo "active"; } ?>">Panel de control</a>
-            <a href="<?php echo get_site_url(); ?>/mi-cuenta/?slug=lista_servicios" class="list-group-item list-group-item-action <?php if(isset($_GET["slug"]) && $_GET["slug"] == "lista_servicios" || isset($_GET["slug"]) && $_GET["slug"] == "add_new" || isset($_GET["slug"]) && $_GET["slug"] == "edit_post"){ echo "active"; } ?>">Lista servicios</a>
+            <a href="<?php echo get_site_url(); ?>/mi-cuenta/?slug=lista_servicios" class="list-group-item list-group-item-action <?php if(isset($_GET["slug"]) && $_GET["slug"] == "lista_servicios" || isset($_GET["slug"]) && $_GET["slug"] == "add_new" || isset($_GET["slug"]) && $_GET["slug"] == "edit_post"){ echo "active"; } ?>">Lista servicios solicitados</a>
             <a href="<?php echo get_site_url(); ?>/mi-cuenta/?slug=propuestas_recibidas" class="list-group-item list-group-item-action <?php if(isset($_GET["slug"]) && $_GET["slug"] == "propuestas_recibidas"){ echo "active"; } ?>">Propuestas recibidas</a>
             <a href="<?php echo get_site_url(); ?>/mi-cuenta/?slug=propuestas_enviadas" class="list-group-item list-group-item-action <?php if(isset($_GET["slug"]) && $_GET["slug"] == "propuestas_enviadas"){ echo "active"; } ?>">Propuestas enviadas</a>
             <a href="<?php echo get_site_url(); ?>/mi-cuenta/?slug=mis_finanzas" class="list-group-item list-group-item-action <?php if(isset($_GET["slug"]) && $_GET["slug"] == "mis_finanzas"){ echo "active"; } ?>">Mis finanzas</a>
             <a href="<?php echo get_site_url(); ?>/mi-cuenta/?slug=editar_perfil" class="list-group-item list-group-item-action <?php if(isset($_GET["slug"]) && $_GET["slug"] == "editar_perfil"){ echo "active"; } ?>">Editar perfil</a>
+            <a href="<?php echo get_site_url(); ?>/mi-cuenta/?slug=cambiar_contrasena" class="list-group-item list-group-item-action <?php if(isset($_GET["slug"]) && $_GET["slug"] == "cambiar_contrasena"){ echo "active"; } ?>">Cambiar contraseña</a>
             <a href="<?php echo wp_logout_url(); ?>" class="list-group-item list-group-item-action">Salir</a>
 
             
